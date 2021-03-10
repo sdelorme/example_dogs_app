def168ae744e37219125b6ecb126bb19b5492d43
Seth Delorme <delorme.s.m@gmail.com>
2021-03-09 19:20:28 -0600

    adds Dog model, adds has_many belongs_to conditions in Dog and USer model

---
+++ app/controllers/api/dogs_controller.rb
@@ -0,0 +1,15 @@
+class Api::DogsController < ApplicationController
+  def create
+    dog = Dog.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed]
+    )
+    if current_user
+      dog.save
+      render json: {message: "Dog saved succesfully"}
+    else
+      render json: {alert: "Users must be logged in to add new dog"}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+    post "/dogs" => "dogs#create"
   end
 end
